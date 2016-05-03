aab4144de45bf66185f78f26c8400c3b66072cb6
kalmanb <kalman@movio.co>
2016-05-04 00:03:48 +1200

    Add react and redux routers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+
 
 import App from './layout/App';
 import configureStore from './store/configureStore';
@@ -18,9 +21,13 @@ injectTapEventPlugin();
 
 const store = configureStore();
 
+const history = syncHistoryWithStore(browserHistory, store)
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
--- src/layout/rootReducer.js
+++ src/layout/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux'
+
 import todos from '../todos';
 
 const rootReducer = combineReducers({
   [todos.NAME]: todos.reducer,
+  routing: routerReducer,
 });
 
 export default rootReducer;
